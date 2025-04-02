d36371607eeb94aca87251806e4e64880014e2c2
Mark Dietzer <git@doridian.net>
2025-04-02 15:50:49 -0700

    Get rid of netmask package (#308)

--- src/interposer/net.ts
+++ src/interposer/net.ts
@@ -1,12 +1,34 @@
 import { networkInterfaces } from 'node:os';
-import { Netmask } from 'netmask';
-
 class NetworkInterface {
+    public readonly broadcast: string;
+    public readonly cidrSegments: number[];
+
     // IPv4-only
     public constructor(
         public readonly ipv4: string,
-        public readonly broadcast_addr: string,
-    ) {}
+        public readonly cidr: string,
+    ) {
+        const [addr, netmaskStr] = this.cidr.split('/');
+        if (!addr || !netmaskStr) {
+            throw new Error(`Invalid CIDR ${this.cidr}`);
+        }
+
+        const addrParts = addr.split('.').map((s) => Number.parseInt(s, 10));
+        if (addrParts.length !== 4) {
+            throw new Error(`Invalid CIDR ${this.cidr}`);
+        }
+        this.cidrSegments = addrParts;
+
+        // eslint-disable-next-line no-bitwise, @typescript-eslint/no-non-null-assertion
+        let addrInt = (addrParts[0]! << 24) | (addrParts[1]! << 16) | (addrParts[2]! << 8) | addrParts[3]!;
+        const netmask = Number.parseInt(netmaskStr, 10);
+
+        // eslint-disable-next-line no-bitwise
+        addrInt |= 0xff_ff_ff_ff & ((1 << (32 - netmask)) - 1);
+
+        // eslint-disable-next-line no-bitwise
+        this.broadcast = `${(addrInt >> 24) & 0xff}.${(addrInt >> 16) & 0xff}.${(addrInt >> 8) & 0xff}.${addrInt & 0xff}`;
+    }
 }
 
 export function getNetworkInterfaces(): NetworkInterface[] {
@@ -17,7 +39,7 @@ export function getNetworkInterfaces(): NetworkInterface[] {
                 continue;
             }
 
-            ret.push(new NetworkInterface(netif.address, new Netmask(netif.cidr).broadcast));
+            ret.push(new NetworkInterface(netif.address, netif.cidr));
         }
     }
 
